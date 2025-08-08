b82692a2e4b0aeebf3db831cfa379ac17443b8ea
chiwasushuba <joshua11084@gmail.com>
2025-08-08 12:46:33 +0800

    feat: added getAllUserRole and getAllAdminRole

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -93,7 +93,33 @@ const loginUser = async (req, res) => {
     }
 };
 
+const getAllUserRole = async (req , res) => {
+    try {
+        const users = await User.find({ role: 'user' });
 
+        if(users.length === 0) {
+            return res.status(404).json({ message: 'No users found' });
+        }
+
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }   
+}
+
+const getAllAdminRole = async (req, res) => {
+    try {
+        const users = await User.find({ role: 'admin' });   
+
+        if(users.length === 0) {
+            return res.status(404).json({ message: 'No admin users found' });
+        }
+
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
 
 
 module.exports = {
