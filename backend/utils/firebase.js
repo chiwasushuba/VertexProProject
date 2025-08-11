90336973564971fbdaa29ceea794c5964b5e678e
chiwasushuba <joshua11084@gmail.com>
2025-08-11 11:38:26 +0800

    feat: firebase init

---
+++ backend/utils/firebase.js
@@ -0,0 +1,14 @@
+const admin = require("firebase-admin");
+
+admin.initializeApp({
+    credential: admin.credential.cert({
+        project_id: process.env.FIREBASE_PROJECT_ID,
+        private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+        private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
+    storageBucket: "gs://mernforum-eff8b.firebasestorage.app",
+});
+
+const bucket = admin.storage().bucket();
+module.exports = { bucket };
