c97624bd4b12a8be36cd6acb3c526984ed0c09ee
chiwasushuba <joshua11084@gmail.com>
2025-08-12 19:55:27 +0800

    feat: integrated created funcitons for userController in routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -138,7 +138,20 @@ const unverifyUser = async (req, res) => {
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
+};
 
+const changeUserRole = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { role } = req.body;
+        const user = await User.findByIdAndUpdate(id, { role }, { new: true });
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        res.status(200).json({ message: 'User role updated successfully', user });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 };
 
 module.exports = {
@@ -147,5 +160,10 @@ module.exports = {
     getUsers,
     getUser,
     getAllUserRole,
-    getAllAdminRole
+    getAllAdminRole,
+    updateUser,
+    deleteUser,
+    verifyUser,
+    unverifyUser,
+    changeUserRole
 }
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -9,13 +9,27 @@ const {
     getAllUserRole,
     getAllAdminRole,
     signup,
+    updateUser,
+    deleteUser,
+    verifyUser,
+    unverifyUser,
+    changeUserRole
 } = require('../controllers/userController');
+const requireAuth = require('../middleware/requireAuth');
 
 router.get('/', getUsers);
 router.get('/:id', getUser);
 router.post('/signup', signup);
 router.post('/login', login);
+
+router.use(requireAuth); // below are the endpoints that require authentication
+
+router.patch('/update/:id', updateUser);
+router.delete('/delete/:id', deleteUser);
 router.get('/role/user', getAllUserRole);
 router.get('/role/admin', getAllAdminRole);
+router.patch('/verify/:id', verifyUser);
+router.patch('/unverify/:id', unverifyUser);
+router.patch('/changerole/:id', unverifyUser);
 
 module.exports = router;
